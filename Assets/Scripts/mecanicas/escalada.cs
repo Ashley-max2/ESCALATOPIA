2dca9518474c52f170bc6fd4ad921530bae08859
Oscar Lillo <osergod999@gmail.com>
2025-11-17 20:24:31 +0100

    Agrego la mecanica de escalada

--- Assets/Scripts/mecanicas/escalada.cs
+++ Assets/Scripts/mecanicas/escalada.cs
@@ -1,18 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class escalada : MonoBehaviour
+public class Escalada : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidadEscalada = 3f;
+    private bool puedeEscalar = false;
+    private bool escalando = false;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Si puede escalar y el jugador pulsa una tecla
+        if (puedeEscalar && Input.GetKey(KeyCode.E))
+        {
+            IniciarEscalada();
+        }
+
+        // Mientras escala
+        if (escalando)
+        {
+            Escalar();
+
+            // Si suelta la tecla, deja de escalar
+            if (!Input.GetKey(KeyCode.E))
+            {
+                PararEscalada();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("escalable"))
+        {
+            puedeEscalar = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("escalable"))
+        {
+            puedeEscalar = false;
+            PararEscalada();
+        }
+    }
+
+    void IniciarEscalada()
+    {
+        escalando = true;
+        rb.useGravity = false;   // desactivamos gravedad
+        rb.velocity = Vector3.zero;
+    }
+
+    void Escalar()
+    {
+        // Movimiento vertical hacia arriba
+        transform.Translate(Vector3.up * velocidadEscalada * Time.deltaTime);
+    }
+
+    void PararEscalada()
+    {
+        escalando = false;
+        rb.useGravity = true;    // recuperar gravedad
     }
 }
