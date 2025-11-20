444fcddaab688d8f90e1c49e21e3a016725cb508
Alejandro Baldeon E <a24josbalesp@inspedralbes.cat>
2025-11-20 20:36:07 +0100

    Todos los estados añadidos

---
+++ Assets/Scripts/FSM/States/JumpingState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player States/Jumping")]
+public class JumpingState : PlayerStateSO
+{
+    public override void Enter(PlayerStateMachine fsm)
+    {
+
+    }
+
+    public override void Exit(PlayerStateMachine fsm)
+    {
+
+    }
+
+    public override void Decide(PlayerStateMachine fsm)
+    {
+
+    }
+}
---
+++ Assets/Scripts/FSM/States/WallJumpingState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player States/Wall Jump")]
+public class WallJumpingState : PlayerStateSO
+{
+    public override void Enter(PlayerStateMachine fsm)
+    {
+        
+    }
+
+    public override void Exit(PlayerStateMachine fsm)
+    {
+        
+    }
+
+    public override void Decide(PlayerStateMachine fsm)
+    {
+        
+    }
+
+}
