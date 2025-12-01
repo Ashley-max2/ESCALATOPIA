9f9f7511f2da5da35f58f7f47eaecb0e0f19ff39
Oscar Lillo <osergod999@gmail.com>
2025-12-01 20:38:04 +0100

    Actualizo la estetica del main menu y arreglo el exitButton

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -7,12 +7,15 @@ public class MainMenu : MonoBehaviour
 {
     public void ChangeScene(string newScene)
     {
+        Debug.Log("Cargando escena: " + newScene);
         SceneManager.LoadScene(newScene);
     }
 
     public void ExitGame()
     {
         Debug.Log("Saliste");
+        
         Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
