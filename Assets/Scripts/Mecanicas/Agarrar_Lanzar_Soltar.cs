77a82a943fedfa78badbc783ee51c21b8143333f
Oscar Lillo <osergod999@gmail.com>
2026-04-04 16:38:48 +0200

    Arreglo errores al detectar el suelo de los barriles en el script: Agarrar_Lanzar_Soltar.cs del jugador

--- Assets/Scripts/Mecanicas/Agarrar_Lanzar_Soltar.cs
+++ Assets/Scripts/Mecanicas/Agarrar_Lanzar_Soltar.cs
@@ -14,13 +14,11 @@ public class AgarrarLanzarSoltar : MonoBehaviour
     private GameObject objetoActual;
     private Rigidbody rbActual;
 
-    // Se ejecuta al iniciar
     void Awake()
     {
         if (cam == null) cam = Camera.main;
     }
 
-    // Input + mantener objeto en mano
     void Update()
     {
         Debug.DrawRay(cam.transform.position, cam.transform.forward * distanciaMax, Color.red);
@@ -48,7 +46,6 @@ public class AgarrarLanzarSoltar : MonoBehaviour
         }
     }
 
-    // Detecta objeto con raycast
     void IntentarAgarrar()
     {
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
@@ -59,7 +56,6 @@ public class AgarrarLanzarSoltar : MonoBehaviour
         }
     }
 
-    // Agarra el objeto
     void AgarrarObjeto(GameObject obj)
     {
         objetoActual = obj;
@@ -76,7 +72,6 @@ public class AgarrarLanzarSoltar : MonoBehaviour
         obj.transform.SetParent(followPoint);
     }
 
-    // Lanza el objeto
     void LanzarObjeto()
     {
         if (rbActual == null) return;
@@ -88,18 +83,24 @@ public class AgarrarLanzarSoltar : MonoBehaviour
 
         rbActual.AddForce(cam.transform.forward * fuerzaLanzamiento, ForceMode.Impulse);
 
-        // Marcar el objeto como lanzado para que se destruya al tocar el suelo
-        DetectarColisionSuelo scriptColision = objetoActual.GetComponent<DetectarColisionSuelo>();
-        if (scriptColision != null)
+        // BUSCAR ThrownBox (en objeto, hijo o padre)
+        ThrownBox thrown = objetoActual.GetComponent<ThrownBox>();
+
+        if (thrown == null)
+            thrown = objetoActual.GetComponentInChildren<ThrownBox>();
+
+        if (thrown == null)
+            thrown = objetoActual.GetComponentInParent<ThrownBox>();
+
+        if (thrown != null)
         {
-            scriptColision.MarcarComoLanzado();
+            thrown.MarcarComoLanzado();
         }
 
         objetoActual = null;
         rbActual = null;
     }
 
-    // Suelta el objeto sin lanzarlo
     void SoltarObjeto()
     {
         if (objetoActual == null) return;
