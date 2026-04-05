1c5afd3b48c326b2637f82e3703052f4ad9270d1
Oscar Lillo <osergod999@gmail.com>
2026-04-05 14:31:31 +0200

    Segunda parte del puzzle/tutorial terminada: ahora la pieza del mecanismo que te suelta el barril sorpresa se puede colocar en el mecanismo del gancho

--- Assets/Scripts/Props/PiezaMecanismo.cs
+++ Assets/Scripts/Props/PiezaMecanismo.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PiezaMecanismo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool tieneElJugador = false;
+
+    [Header("Interacción")]
+    public float distanciaInteraccion = 3f;
+    public GameObject objetoAActivar; // Lo asignas desde el inspector
+
+    private Camera camara;
+
+    private void Start()
     {
-        
+        camara = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Solo puede interactuar si ya tiene la pieza
+        if (tieneElJugador && Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = new Ray(camara.transform.position, camara.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, distanciaInteraccion))
+            {
+                // Detectar por tag "mecanismo"
+                if (hit.collider.CompareTag("mecanismo"))
+                {
+                    if (objetoAActivar != null)
+                    {
+                        objetoAActivar.SetActive(true);
+                    }
+
+                    // Aquí termina la lógica de este script
+                    enabled = false;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !tieneElJugador)
+        {
+            tieneElJugador = true;
+
+            transform.SetParent(other.transform);
+            transform.localPosition = Vector3.zero;
+
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)
+                rend.enabled = false;
+
+            Collider col = GetComponent<Collider>();
+            if (col != null)
+                col.enabled = false;
+        }
     }
 }
