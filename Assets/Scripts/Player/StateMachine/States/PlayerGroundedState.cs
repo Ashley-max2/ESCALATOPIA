27bd0d601621db0dd1f81091513fdf8f82fe6060
Alejandro Baldeon <a24josbalesp@inspedralbes.cat>
2026-04-15 01:28:07 +0200

    Cambios de scripts de animaciones

--- Assets/Scripts/Player/StateMachine/States/PlayerGroundedState.cs
+++ Assets/Scripts/Player/StateMachine/States/PlayerGroundedState.cs
@@ -16,6 +16,9 @@ public class PlayerGroundedState : PlayerBaseState
         ctx.IsClimbing = false;
         ctx.CurrentVelocity = new Vector3(ctx.Rb.velocity.x, 0, ctx.Rb.velocity.z);
         
+        // Play end jump animation when landing
+        ctx.Animator.Play("JumpEnd");
+        
         // Check for fall damage
         if (ctx.FallStartHeight > 0)
         {
