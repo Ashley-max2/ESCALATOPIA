277d6d814acb1577a437f9d810de09754f4289cb
Oscar Lillo <osergod999@gmail.com>
2026-05-08 19:11:30 +0200

    Manager para recoger el gancho y para no poder cruzar la montaña sin él

---
+++ Assets/Scripts/Props/HookManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HookManager : MonoBehaviour
+{
+    public MeshRenderer[] meshes;
+    public GameObject objectToDelete1;
+    public GameObject objectToDelete2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Hacer invisibles todas las meshes
+            foreach (MeshRenderer mesh in meshes)
+            {
+                mesh.enabled = false;
+            }
+
+            // Borrar los otros dos objetos
+            Destroy(objectToDelete1);
+            Destroy(objectToDelete2);
+        }
+    }
+}
